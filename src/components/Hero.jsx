5f3c036c8c2b7b3228ef719b501281e4ce30f8e8
Divyanshu7611 <dshapbss202@gmail.com>
2024-04-26 02:58:35 +0530

    hero ui

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,23 @@
 import React from "react";
 import { IoLogoGooglePlaystore } from "react-icons/io5";
 import { SiAppstore } from "react-icons/si";
+import Image from "next/image";
 
 export default function Hero() {
   return (
     <div>
-      <div className="hero min-h-screen min-w-screen flex items-center justify-center">
-        <div className="flex flex-col justify-center items-center max-w-[70rem]">
-          <img src="/logo.png" alt="" className="h-[100px]" />
-          <h1 className="text-center lg:text-7xl text-2xl font-bold">
-            Utkarsh Kisan Revolutionizing
+      <div className="hero min-h-screen min-w-screen flex overflow-hidden">
+        <div className="flex flex-col lg:justify-start items-center lg:items-start lg:max-w-[70rem] my-auto lg:ml-10 m-3 w-fit">
+          <h1 className="lg:text-7xl text-2xl font-bold text-[#b1c445] heading font-serif">
+            Sow. Connect. Harvest.
           </h1>
-          <h1 className="text-center lg:text-5xl text-xl font-bold">
-            Agriculture with techhnology
+          <h1 className="lg:text-2xl text-md font-bold text-center mt-5">
+            Utkarsh Kisan Revolutionizing Agriculture with techhnology
           </h1>
-          <p className="max-w-lg text-center mt-3">
-            Li Europan lingues es membres del sam familie. Lor separat
-            existentie es un myth. Por scientie, musica, sport etc, litot Europa
-            usa li sam vocabular.
-          </p>
-          <p className="mt-10">Download Utkarsh Kisan App</p>
-          <div className="flex lg:flex-row flex-col gap-7 mt-5">
+          <p></p>
+
+          <p className="mt-5">Download Utkarsh Kisan App</p>
+          <div className="flex lg:flex-row flex-col gap-7 mt-5 w-full lg:w-fit">
             <button className="bg-[#b1c445] py-3 px-5 rounded-lg font-semibold hover:scale-105">
               Download Now
             </button>
@@ -28,6 +25,15 @@ export default function Hero() {
               Read More
             </button>
           </div>
+        </div>
+        <div className="absolute right-0 bottom-0 animation overflow-hidden">
+          <Image
+            alt=""
+            src={"/static/phone.png"}
+            className="lg:h-[500px] lg:w-[500px] h-[10rem] w-[10rem]"
+            height={400}
+            width={400}
+          />
         </div>
       </div>
     </div>
--- src/components/Hero2.jsx
+++ src/components/Hero2.jsx
@@ -5,8 +5,8 @@ import autoprefixer from "autoprefixer";
 function hero2() {
   return (
     <div>
-      <div className="max-w-[1200px] flex lg:flex-row flex-col mx-auto p-10 justify-between">
-        <div className="lg:w-1/2 flex flex-col justify-center gap-5">
+      <div className="max-w-[1200px] flex lg:flex-row flex-col mx-auto lg:p-10 justify-between">
+        <div className="lg:w-1/2 flex flex-col justify-center gap-5 p-8 lg:p-0">
           <h1 className="lg:text-5xl text-2xl font-bold text-[#6f7a2d]">
             Our Passion for Agriculture Nurturing Growth and Sustaining the
             Future
@@ -22,9 +22,9 @@ function hero2() {
             </button>
           </div>
         </div>
-        <div className="lg:w-1/2  flex flex-col relative">
-          <img src="/static/img2.png" className="relative" />
-          <div className="absolute px-10 py-5 rounded-xl bg-[#b1c445] bottom-0 left-1/4 lg:-translate-y-8">
+        <div className="lg:w-1/2  flex flex-col relative w-full mx-auto">
+          <img src="/static/img2.png" className="relative mx-auto" />
+          <div className="absolute px-10 py-5 rounded-xl opacity-0 lg:opacity-100 bg-[#b1c445] bottom-0 lg:left-1/4 lg:-translate-y-8">
             <h1 className="text-[#204E51] font-bold text-4xl">Since 1983</h1>
           </div>
         </div>
